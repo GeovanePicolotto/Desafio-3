7f7eabc939b8d273c5864a3876d23ebb58ab86b9
Geovane Picolotto <geovanedallpuppop@outlook.com>
2024-07-23 20:23:58 -0300

    feat: adding header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,14 @@ import NavBar from "./NavBar"
 
 const Header = () => {
   return (
-    <header className="w-screen h-24 border border-black">
-        <div className="flex">
+    <header className="flex justify-center items-center w-screen h-24">
+        <div className="flex gap-4 w-10/12 h-10 justify-between">
             <img src="/src/imgs/furniro_logo.png" alt="Furniro Logo" />
             <NavBar/>
+            <div className="flex justify-center items-center w-24 gap-4">
+              <a href=""><img src="/src/imgs/userLogo.png" alt="Icone usuario" /></a>
+              <a href=""><img src="/src/imgs/shopIcon.png" alt="Icone carrinho de compras" /></a>
+            </div>
         </div>
     </header>
   )
