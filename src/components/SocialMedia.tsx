44373188fb54e0af4459cbb4d4c5a5016fb47fcc
Geovane Picolotto <geovanedallpuppop@outlook.com>
2024-07-25 14:33:05 -0300

    feat: footer style

---
+++ src/components/SocialMedia.tsx
@@ -0,0 +1,12 @@
+const SocialMedia = () => {
+  return (
+    <div className="flex gap-7">
+      <button className="flex justify-center items-center w-9 h-9 m-a rounded-full shadow-lg "><img src="/src/imgs/facebook 1.png" alt="" /></button>
+      <button className="flex justify-center items-center w-9 h-9 m-a rounded-full shadow-lg "><img src="/src/imgs/instagram 1.png" alt="" /></button>
+      <button className="flex justify-center items-center w-9 h-9 m-a rounded-full shadow-lg "><img src="/src/imgs/twitter 1.png" alt="" /></button>
+      <button className="flex justify-center items-center w-9 h-9 m-a rounded-full shadow-lg "><img src="/src/imgs/linkedin 1.png" alt="" /></button>
+    </div>
+  )
+}
+
+export default SocialMedia
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
+import Footer from "../components/Footer"
 import Header from "../components/Header"
 
 const Home = () => {
   return (
     <div>
         <Header/>
-        <h1 className="text-7xl"> HOME </h1>
+        <h1 className="text-7xl h-[500px]"> HOME </h1>
+        <Footer/>
         
     </div>
   )
