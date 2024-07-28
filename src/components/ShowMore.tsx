6df6b220fe2503d31183a3fcb5ddd064fb9c492e
Geovane Picolotto <geovanedallpuppop@outlook.com>
2024-07-28 01:46:36 -0300

    Fixing the Show More button

--- src/components/ShowMore.tsx
+++ src/components/ShowMore.tsx
@@ -2,9 +2,12 @@ import { Link } from "react-router-dom"
 
 const ShowMore = () => {
   return (
-    <div className="flex mt-10 justify-center items-center w-[245px] h-[48px] border border-amareloQueimado text-amareloQueimado font-semibold"  style={{ fontFamily: 'Poppins, sans-serif', fontSize: '16px' }}>
-        <Link to={"/shop"}>Show More</Link>
-    </div>
+   
+    <Link to={"/shop"}>
+        <div className="flex mt-10 justify-center items-center w-[245px] h-[48px] border border-amareloQueimado text-amareloQueimado font-semibold"  style={{ fontFamily: 'Poppins, sans-serif', fontSize: '16px' }}>Show More 
+        </div>
+    </Link>
+    
   )
 }
 
